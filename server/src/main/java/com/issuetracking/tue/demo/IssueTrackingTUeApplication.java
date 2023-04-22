1bd234da539e4548952d8f1d8707e5e7d68631a8
Amin Bakhshi <bakhshiamin85@gmail.com>
2023-04-22 21:51:50 +0200

    feat(server): init repository

---
+++ server/src/main/java/com/issuetracking/tue/demo/IssueTrackingTUeApplication.java
@@ -0,0 +1,13 @@
+package com.issuetracking.tue.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingTUeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingTUeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/issuetracking/tue/demo/IssueTrackingTUeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issuetracking.tue.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingTUeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
